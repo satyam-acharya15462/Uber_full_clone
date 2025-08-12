0f3ca149d1847f2d19f4b74b71fbfc9a2b4c0e34
satyam-acharya15462 <satyam.acharya2024@gmail.com>
2025-08-12 23:04:54 +0530

    created pages completed the home page and the user login page have not work on the responsiveness of the website

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import UserSignUp from './pages/UserSignUp'
+import UserLogin from './pages/UserLogin'
+import CaptainLogin from './pages/CaptainLogin'
+import CaptainSignUp from './pages/CaptainSignUp'
 
 const App = () => {
   return (
     <div>
-      
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<UserLogin/>}/>
+        <Route path='/signUp' element={<UserSignUp/>}/>
+        <Route path='/captain-login' element={<CaptainLogin/>}/>
+        <Route path='/captain-signUp' element={<CaptainSignUp/>}/>
+      </Routes>
     </div>
   )
 }
---
+++ Frontend/src/pages/UserLogin.jsx
@@ -0,0 +1,59 @@
+import React , {useState} from 'react'
+import UberLogo from '../assets/Uber_logo_2018.png'
+import { Link ,  } from 'react-router-dom'
+const UserLogin = () => {
+   const [Email, setEmail] = useState('')
+   const [Password , setPassword] = useState('')
+   const SumbitHandler = (e) => {
+    e.preventDefault()
+   }
+
+  return (
+    <div className='p-7 flex justify-between flex-col h-screen'>
+    <div>
+    <img className="w-16 mb-5" src={UberLogo} alt="Uber Logo" />
+      
+      <form onSubmit={(e)=>{
+        SumbitHandler(e)
+      }}>
+
+      <h3 className='text-xl font-bold mb-2'>What's your email</h3>
+
+      <input 
+      className='bg-[#eeeeee]  rounded px-2 py-2  w-full text-lg placeholder:text-base ml-1'
+      type="email" 
+      value={Email}
+      onChange={(e)=>{
+         setEmail(e.target.value)
+      }}
+      required 
+      placeholder='Email@Example.com' 
+      />
+
+      <h3 className='text-xl mb-2 mt-4 font-bold'>Enter password</h3>
+
+      <input 
+      type="password" 
+      required  
+      value={Password}
+      onChange={
+        (e)=>{
+          setPassword(e.target.value)
+        }
+      }
+      placeholder='password'
+      className='bg-[#eeee] rounded px-2 py-2  w-full text-lg placeholder:text-base ml-1'
+      />
+
+      <button className='bg-black text-white font-semibold mt-7 rounded px-2 py-2  w-full text-lg placeholder:text-base'>Login</button>
+      </form>
+       <p className='text-center mt-4'>New here? <Link className='text-blue-800' to='/signUp'>Create New Account</Link></p>
+    </div> 
+    <div>
+    <button className='bg-[#2E7D32] text-white font-semibold mt-1 rounded px-2 py-2  w-full text-lg placeholder:text-base'>sign in as captain</button>
+    </div>
+    </div>
+  )
+}
+
+export default UserLogin
